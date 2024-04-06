10c3a17b6a688cf6a158996f6548a2a1f6cfa6f1
Zubair Ahmed Balouch <zubairahmedbalouch37@gmail.com>
2024-04-06 18:46:49 +0500

    push my simple-cli-calculator to GitHub

---
+++ index.js
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter your first Number", type: "number", name: "firstNumber" },
+    { message: "Enter your Second Number", type: "number", name: "secondNumber" },
+    { message: "Select One of the Operator to perform Operation",
+        type: "list",
+        name: "Operator",
+        choices: ["Addition", "Subtration", "Multiplication", "Division"],
+    },
+]);
+if (answer.Operator === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.Operator === "Subtration") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.Operator === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.Operator === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+    console.log("Please select a valid Operator");
+}
